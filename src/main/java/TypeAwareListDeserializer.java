12fe4c27588896ee0c0502d014e385f373b1f0fb
forax <forax@univ-mlv.fr>
2025-07-14 16:39:22 +0200

    initial commit

---
+++ src/main/java/TypeAwareListDeserializer.java
@@ -0,0 +1,48 @@
+import com.fasterxml.jackson.core.JsonParser;
+import com.fasterxml.jackson.core.JsonToken;
+import com.fasterxml.jackson.databind.BeanProperty;
+import com.fasterxml.jackson.databind.DeserializationContext;
+import com.fasterxml.jackson.databind.JavaType;
+import com.fasterxml.jackson.databind.JsonDeserializer;
+import com.fasterxml.jackson.databind.deser.ContextualDeserializer;
+import com.fasterxml.jackson.databind.deser.std.StdDeserializer;
+
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+
+final class TypeAwareListDeserializer extends StdDeserializer<List<?>> implements ContextualDeserializer {
+  TypeAwareListDeserializer(JavaType type) {
+    super(type);
+  }
+
+  private List<Object> createList(JavaType contentType) {
+    var rawClass = contentType.getRawClass();
+    if (rawClass.isValue()) {
+      return new ValueList<>(rawClass);
+    }
+    return new ArrayList<>();
+  }
+
+  @Override
+  public List<?> deserialize(JsonParser p, DeserializationContext ctxt) throws IOException {
+    var targetType = getValueType();
+    var contentType = targetType.getContentType();
+
+    var list = createList(contentType);
+    if (p.isExpectedStartArrayToken()) {
+      var elementDeserializer = ctxt.findRootValueDeserializer(contentType);
+
+      while (p.nextToken() != JsonToken.END_ARRAY) {
+        var element = elementDeserializer.deserialize(p, ctxt);
+        list.add(element);
+      }
+    }
+    return list;
+  }
+
+  @Override
+  public JsonDeserializer<?> createContextual(DeserializationContext ctxt, BeanProperty property) {
+    return new TypeAwareListDeserializer(property.getType());
+  }
+}
---
+++ src/main/java/ValueList.java
@@ -0,0 +1,38 @@
+import java.lang.reflect.Array;
+import java.util.AbstractList;
+import java.util.Arrays;
+import java.util.Objects;
+
+final class ValueList<E> extends AbstractList<E> {
+  private E[] values;
+  private int size;
+
+  @SuppressWarnings("unchecked")
+  public ValueList(Class<?> valueClass) {
+    if (!valueClass.isValue()) {
+      throw new IllegalArgumentException("must be a value class");
+    }
+    this.values = (E[]) Array.newInstance(valueClass, 0);
+  }
+
+  @Override
+  public int size() {
+    return size;
+  }
+
+  @Override
+  @SuppressWarnings("unchecked")
+  public E get(int index) {
+    Objects.checkIndex(index, size);
+    return (E) Array.get(values, index);
+  }
+
+  @Override
+  public boolean add(E element) {
+    if (size == values.length) {
+      values = Arrays.copyOf(values, Math.max(16, values.length << 1));
+    }
+    Array.set(values, size++, element);
+    return true;
+  }
+}
