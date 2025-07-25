eb78805073495f0c7bee65d8f8fcd81e1213126f
forax <forax@univ-mlv.fr>
2025-07-25 15:39:16 +0200

    main() can use Java 25 allowed signatures

--- src/main/java/weather/WeatherExample.java
+++ src/main/java/weather/WeatherExample.java
@@ -3,17 +3,19 @@ package weather;
 import java.io.IOException;
 import java.time.LocalDate;
 
+// --enable-preview --add-exports=java.base/jdk.internal.value=ALL-UNNAMED --add-exports=java.base/jdk.internal.misc=ALL-UNNAMED
 class WeatherExample {
-  public static void main(String[] args) throws IOException {
-    // Paris coordinates
+  static void main() throws IOException {
+    // Coordinates
     var paris = new WeatherService.LatLong(48.864716, 2.349014);
+    var noirmoutier = new WeatherService.LatLong(46.966667, -2.216667);
+    var toulon = new WeatherService.LatLong(43.125833, 5.930556);
 
     // One year, 2024-2025
     var endDate = LocalDate.parse("2025-01-01");
     var startDate = endDate.minusYears(1);
 
     var weatherData = WeatherService.getWeatherData(paris, startDate, endDate);
-
     System.out.println(WeatherComputation.compute(weatherData));
   }
 }
